92791fe94788141ec8a4146678f6d5c2f45cfd57
Yash Kanekar <kanekaryash.11@gmail.com>
2024-11-07 07:56:58 +0530

    initalizing the project and setting up basic routes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import PlaygroundScreen from "./screens/PlaygroundScreen";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/playground" element={<PlaygroundScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/screens/PlaygroundScreen/index.jsx
@@ -0,0 +1,5 @@
+const PlaygroundScreen = () => {
+  return <div>PlaygroundScreen</div>;
+};
+
+export default PlaygroundScreen;
