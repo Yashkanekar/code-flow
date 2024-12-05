91d6a2ffad759ad1c6b2f2da03d19a03c2d63b05
yashk <kanekaryash.11@gmail.com>
2024-12-05 15:12:45 +0530

    feat: completed home screen UI for left section

--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -1,5 +1,23 @@
+import "./index.scss";
+
 const HomeScreen = () => {
-  return <div>HomeScreen</div>;
+  return (
+    <div className="home-container">
+      <div className="left-container">
+        <div className="items-container">
+          <img src="../../../assets/code.png" />
+          <h1>Code Flow</h1>
+          <button>
+            <span className="material-icons">add</span>
+            <span>Create Playground</span>
+          </button>
+        </div>
+      </div>
+      <div className="right-container">
+        <h1>Right container</h1>
+      </div>
+    </div>
+  );
 };
 
 export default HomeScreen;
